f03f74150336091dcd00c2ca7929fecabf0b001c
Mishele123 <misha228665161@gmail.com>
2023-10-24 18:14:18 +0400

    function scrapeImages for low pictures

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
-import os
-from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import time
+import requests
 
-def get_proxy(filename : str) -> []:
-    file = open(filename, "r", encoding="utf-8")
-    proxy = [line.strip() for line in file]
-    return proxy
 
-def downloadImage(text: str, proxy: []) -> set:
-    ua = UserAgent()
-    arr = set()
-    max_count = 1100  # Минимально количество изображений для загрузки
-    main_url = "https://yandex.ru/images/search?text=" + text + "&p="
-    for a in range(1, 100 + 1):   # На одной странице 30 картинок, поэтому должно хватить :D
-        print(main_url + str(a))
-        result = requests.get(main_url + str(a), headers=
-                            {
-                                "User-Agent" : str(ua.random)
-                            },
-                            proxies={
-                                "http" : "http://" + proxy[a - 1]
-                            })
-            
-        soup = BeautifulSoup(result.content, "lxml")
-        links = soup.findAll("img", class_ = "serp-item__thumb justifier__thumb")
-        print(links)
-        for link in links:
-            link = link.get("src")
-            print(link)
-            arr.add(link)
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 500)
 
-        if len(arr) > max_count:
+def scrapeImages(key: str):
+    max_count = 1100
+    numbers_of_scrolls = max_count / 300 + 1
+    print(type(numbers_of_scrolls))
+    url = f"https://yandex.ru/images/search?text={key}"
+    driver.get(url=url)
+
+    downloaded_img_count = 0
+    for x in range(int(numbers_of_scrolls)):
+        for xx in range(10):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(10)
+        time.sleep(10)
+
+        try:
+            driver.find_element("xpath", "/html/body/div[4]/div[2]/div/div[2]/a").click()
+        except:
             break
-        time.sleep(5)
-    return arr
+        
+    images = driver.find_elements(By.CSS_SELECTOR, "img.serp-item__thumb")
+    time.sleep(10)
+    print(f"Найденно изображений: {len(images)}")
+    
+    for img in images:
+        img_url = img.get_attribute("src")
+        if img_url:
+            print(img_url)
+    
+    time.sleep(100)
 
+    driver.close()
+    driver.quit()
 
 def main():
-    arr = ["tiger_big", "leopard_big", "tiger_small", "leopard_small"]
-    downloadImage(arr[0], get_proxy("proxy.txt"))
+
+    scrapeImages("tiger")
+
 
 if __name__ == "__main__":
     main()
